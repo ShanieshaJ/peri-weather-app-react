b4b9a2086bab57b76066ee1b11d3f0b5a3fe771d
Shaniesha John <shaniesha.john.94@gmail.com>
2022-03-25 12:33:02 -0400

    Removed the import bootstrap and edited the code

--- src/WeatherForm.js
+++ src/WeatherForm.js
@@ -1,25 +1,25 @@
 import React from "react";
-
-import "./WeatherForm.css";
-import "bootstrap/dist/css/bootstrap.css";
 import CurrentData from "./CurrentData";
 import CurrentWeatherData from "./CurrentWeatherData";
+
+import "./WeatherForm.css";
 
 export default function WeatherForm() {
   return (
     <div className="WeatherForm">
       <form className="mb-3">
         <div className="row">
-          <div className="col search-form">
+          <div className="col-9 search-form">
             <input
               type="search"
               className="form-control"
               autoFocus="on"
+              autoComplete="off"
               id="inlineFormInputName"
               placeholder="Search for a city"
             />
           </div>
-          <div className="col-5 search-location-area">
+          <div className="col-3 search-location-area">
             <button type="search" className="btn btn-primary pull-right">
               Search
             </button>
