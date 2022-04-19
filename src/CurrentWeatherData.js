c5e0fb124bca6d45bc35df02b70d36a6790876a4
Shaniesha John <shaniesha.john.94@gmail.com>
2022-04-19 01:00:05 -0400

    Made changes to this file

--- src/CurrentWeatherData.js
+++ src/CurrentWeatherData.js
@@ -12,7 +12,7 @@ export default function CurrentWeatherData() {
   return (
     <div className="CurrentWeatherData">
       <div className="row current-weather-container">
-        <div className="col current-weather">
+        <div className="col-6 current-weather-img">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
             alt="clear"
@@ -20,46 +20,32 @@ export default function CurrentWeatherData() {
             className="float-left"
           />
         </div>
-        <div className="row current-weather-container">
-          <div className="col current-temperature">
-            <span id="current-temperature">
-              {currentTemperatureInfo.currentTemperature}{" "}
-            </span>
-            <span className="temperature-dot">º</span>
-            <span className="units">
-              <a href="/" id="celsius">
-                {" "}
-                ºC
-              </a>
-              <a href="/" id="fahrenheit">
-                {" "}
-                / ºF
-              </a>
-            </span>
-          </div>
-
-          <div className="row current-weather-container">
-            <div className="col weather-humidity-wind-container">
-              <div className="weather">
-                {currentTemperatureInfo.currentDescription}
-              </div>
-              <div className="humidity">
-                Humidity:{" "}
-                <span id="current-humidity">
-                  {currentTemperatureInfo.currentHumidity}
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix current-weather-temperature">
+              <div className="float-left">
+                <strong>{currentTemperatureInfo.currentTemperature}</strong>
+                <span className="units">
+                  <a href="/" id="celsius">
+                    °C
+                  </a>{" "}
+                  |{" "}
+                  <a href="/" id="fahrenheit">
+                    °F
+                  </a>
                 </span>
-                %
-              </div>
-              <div className="wind">
-                Wind:{" "}
-                <span id="wind-speed">
-                  {currentTemperatureInfo.currentWindSpeed}
-                </span>{" "}
-                km/h
               </div>
             </div>
           </div>
+          <div className="col-6">
+            <ul>
+              <li>Description: {currentTemperatureInfo.currentDescription}</li>
+              <li>Humidity: {currentTemperatureInfo.currentHumidity}%</li>
+              <li>Wind: {currentTemperatureInfo.currentWindSpeed} km/h</li>
+            </ul>
+          </div>
         </div>
+        <div className="weather-forecast" id="forecast"></div>
       </div>
     </div>
   );
