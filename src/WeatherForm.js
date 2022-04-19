2ac6144cb52404313ff0dd9a21c9317f03560b7b
Shaniesha John <shaniesha.john.94@gmail.com>
2022-04-19 10:27:31 -0400

    Imported the FontAwesome icons

--- src/WeatherForm.js
+++ src/WeatherForm.js
@@ -1,6 +1,11 @@
 import React from "react";
 import CurrentData from "./CurrentData";
 import CurrentWeatherData from "./CurrentWeatherData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationCrosshairs,
+  faMagnifyingGlassLocation,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./WeatherForm.css";
 
@@ -20,15 +25,15 @@ export default function WeatherForm() {
             />
           </div>
           <div className="col-3 search-location-area">
-            <button type="search" className="btn btn-primary pull-right w-100">
-              Search
+            <button type="search" className="btn btn-primary pull-right">
+              <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
             </button>
             <button
               type="button"
               className="btn btn-primary pull-right"
               id="current-location"
             >
-              Current
+              <FontAwesomeIcon icon={faLocationCrosshairs} />
             </button>
           </div>
         </div>
